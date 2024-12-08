71281ef3034265dfdbc504ba058a7259140217b4
cathartyc <67878876+cathartyc@users.noreply.github.com>
2024-12-09 00:57:19 +0100

    add puzzle n.8 2024

---
+++ 2024/aoc08/src/bin/part2.rs
@@ -0,0 +1,96 @@
+use std::collections::{HashMap, HashSet};
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+struct Loc {
+    x: isize,
+    y: isize,
+}
+
+fn is_in_bound(val: &Loc, bound_x: usize, bound_y: usize) -> bool {
+    val.x >= 0 && val.x <= bound_x as isize && val.y >= 0 && val.y <= bound_y as isize
+}
+
+fn find_antinodes(ant_1: &Loc, ant_2: &Loc, bound_x: usize, bound_y: usize) -> Vec<Loc> {
+    let mut antinodes: Vec<Loc> = vec![];
+    let dist_x = ant_2.x - ant_1.x;
+    let dist_y = ant_2.y - ant_1.y;
+    let mut antinode_1 = Loc {
+        x: ant_1.x - dist_x,
+        y: ant_1.y - dist_y,
+    };
+    loop {
+        if is_in_bound(&antinode_1, bound_x, bound_y) {
+            antinodes.push(antinode_1);
+        } else {
+            break;
+        }
+        antinode_1.x -= dist_x;
+        antinode_1.y -= dist_y;
+    }
+    let mut antinode_2 = Loc {
+        x: ant_2.x + dist_x,
+        y: ant_2.y + dist_y,
+    };
+    loop {
+        if is_in_bound(&antinode_2, bound_x, bound_y) {
+            antinodes.push(antinode_2);
+        } else {
+            break;
+        }
+        antinode_2.x += dist_x;
+        antinode_2.y += dist_y;
+    }
+    antinodes
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("../inputs/input8.txt");
+    let input = fs::read_to_string(path)?;
+    // Code
+    let mut antennas: HashMap<char, Vec<Loc>> = HashMap::new();
+    let mut antinodes: HashSet<Loc> = HashSet::new();
+    let mut size_x: usize = 0;
+    let mut size_y: usize = 0;
+    for (x, line) in input.lines().enumerate() {
+        size_x += 1;
+        if size_y == 0 {
+            size_y = line.chars().count();
+        }
+        for (y, ch) in line.chars().enumerate() {
+            match ch {
+                '.' => continue,
+                _ => antennas
+                    .entry(ch)
+                    .and_modify(|vec| {
+                        vec.push(Loc {
+                            x: x as isize,
+                            y: y as isize,
+                        })
+                    })
+                    .or_insert(vec![Loc {
+                        x: x as isize,
+                        y: y as isize,
+                    }]),
+            };
+        }
+    }
+    // make comparisons less error-prone
+    size_x -= 1;
+    size_y -= 1;
+    for antennas in antennas.values() {
+        if antennas.len() > 1 {
+            antinodes.extend(antennas);
+        }
+        for (i, ant_1) in antennas.iter().enumerate() {
+            for ant_2 in &antennas[i + 1..] {
+                antinodes.extend(find_antinodes(ant_1, ant_2, size_x, size_y));
+            }
+        }
+    }
+    println!("{}", antinodes.len());
+    Ok(())
+}
